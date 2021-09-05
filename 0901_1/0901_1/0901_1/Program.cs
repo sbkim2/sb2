077d3a9793aeb1d493577e54eeb79fb1147afe22
sbkim2 <sbkim2@sw.co.kr>
2021-09-06 08:46:40 +0900

    2

--- 0901_1/0901_1/0901_1/Program.cs
+++ 0901_1/0901_1/0901_1/Program.cs
@@ -7,18 +7,40 @@ namespace _0901_1
     {
         static void Main(string[] args)
         {
-            List<string> list = new List<string>();
-            list.Add("박나나");
-            list.Add("박정운");
-            list.Add("이윤형");
-            list.Add("조미희");
-            list.Add("유영석");
-            list.Add("김상범");
+            personMode[] shinwon = {new personMode("박나나", "정보시스템팀", "7214"), new personMode("박정운", "정보시스템팀", "7238"), new personMode("이윤형", "정보시스템팀", "7252"),
+            new personMode("조미희", "정보시스템팀", "7262"), new personMode("유영석", "정보시스템팀", "7207"), new personMode("김상범", "정보시스템팀", "7225")};
 
-            foreach (string i in list)
+
+
+
+            for (int i = 0; i <= shinwon.Length; i++)
             {
-                Console.WriteLine(i);
+                shinwon[i].Sinfo();
+                Console.WriteLine();
             }
         }
     }
+
+    class personMode
+    {
+        public string Name;
+        public string Dep;
+        public string Num;
+
+        public personMode(string name, string dep, string num)
+        {
+            this.Name = name;
+            this.Dep = dep;
+            this.Num = num;
+        }
+        public void Sinfo()
+        {
+            Console.WriteLine($"이름 : {Name}");
+            Console.WriteLine($"부서 : {Dep}");
+            Console.WriteLine($"사내번호 : {Num}");
+        }
+
+    }
+    
 }
+

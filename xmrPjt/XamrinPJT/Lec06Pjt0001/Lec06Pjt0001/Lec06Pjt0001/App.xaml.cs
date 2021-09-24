b61cda03642d0b5e27563859466739eeb08adb96
sbkim2 <sbkim2@sw.co.kr>
2021-09-25 01:34:59 +0900

    Layout

---
+++ xmrPjt/XamrinPJT/Lec06Pjt0001/Lec06Pjt0001/Lec06Pjt0001/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lec06Pjt0001
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new FrameEx(); //플렛폼에서 실행할 시 Frame.xaml을 읽어 실행함
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ xmrPjt/XamrinPJT/Lec06Pjt0001/Lec06Pjt0001/Lec06Pjt0001/MyContentView.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lec06Pjt0001
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyContentView : ContentView
+    {
+        public MyContentView()
+        {
+            InitializeComponent(); // 이때 자믈파일에 있는 컴퍼넌트를 메모리에 생성함
+
+            pName.Text = "";
+            pMajor.Text = "";
+            pNation.Text = "";
+            pGender.Text = "";
+            pAge.Text = "";
+        }
+
+        public string Name
+        {
+            get
+            {
+                return pName.Text;
+            }
+            set
+            {
+                pName.Text = value;
+            }
+        }
+        public string Major { 
+            get
+            {
+                return pMajor.Text;
+            }
+            set
+            {
+                pMajor.Text = value;
+            }
+        }
+        public string Nation
+        {
+            get
+            {
+                return pNation.Text;
+            }
+            set
+            {
+                pNation.Text = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return pGender.Text;
+            }
+            set
+            {
+                pGender.Text = value;
+            }
+        }
+        public string Age
+        {
+            get
+            {
+                return pAge.Text;
+            }
+            set
+            {
+                pAge.Text = value;
+            }
+        }
+    }
+}

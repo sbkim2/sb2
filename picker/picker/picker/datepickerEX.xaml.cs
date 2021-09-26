4bde0bb30afa59a3eb4eaae7d7e08a0ba1b708bd
sangbeom <rla789qja@naver.com>
2021-09-26 23:33:06 +0900

    picker

---
+++ picker/picker/picker/datepickerEX.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace picker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class datepickerEX : ContentPage
+    {
+        public datepickerEX()
+        {
+            InitializeComponent();
+        }
+
+        private void dp01_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            var id = ((DatePicker)sender).Id;
+            if (id == dp01.Id)
+            {
+                DateTime dateTime = e.NewDate; // e는 센더로 부터 입력받는 값임
+                int year = dateTime.Year; // 입력한 년도를 가져옴
+                int month = dateTime.Month; //월을 가져옴
+                int day = dateTime.Day; // 일을 가져옴
+
+                char[] days = { '일', '월', '화', '수', '목', '금', '토' }; // day라는 배열에 0부터 6까지 값을 넣어줌
+                char date = days[(int)dateTime.DayOfWeek]; //요일을 가져오는데 0부터 6까지 숫자로 가져옴
+
+                Debug.WriteLine($"{year}년 {month}월 {day}일 {date}요일");
+                label01.Text = $"{year}년 {month}월 {day}일 {date}요일";
+            }
+        }
+    }
+}
---
+++ picker/picker/picker/timepickerEX.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace picker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class timepickerEX : ContentPage
+    {
+        public timepickerEX()
+        {
+            InitializeComponent();
+        }
+
+        private void tp01_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e) // 속성이 변하면 발동하는 핸들러다
+        {
+            var id = ((TimePicker)sender).Id;
+            if (id == tp01.Id) // 해당 id가 tp01의 아이디이면?
+            {
+                if (e.PropertyName == "Time") // e의 속성이 시간이면?
+                {
+                    //label01.Text = tp01.Time.ToString();
+
+                    int h = tp01.Time.Hours;
+                    int m = tp01.Time.Minutes;
+                    int s = tp01.Time.Seconds;
+                    label01.Text = $"{h}시 {m}분 {s}초"; // 시간의 시,분,초 를 인자로 따로따로 받아 출력해줌
+                }
+            }
+        }
+    }
+}

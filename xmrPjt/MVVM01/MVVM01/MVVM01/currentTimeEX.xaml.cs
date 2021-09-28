9337f41d5ba60b6bef1171603736adc6f987cb06
sangbeom <rla789qja@naver.com>
2021-09-28 23:40:55 +0900

    mvvm 23강

---
+++ xmrPjt/MVVM01/MVVM01/MVVM01/currentTimeEX.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class currentTimeEX : ContentPage
+    {
+        public currentTimeEX()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ xmrPjt/MVVM01/MVVM01/MVVM01/currentTimeEX02.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class currentTimeEX02 : ContentPage
+    {
+        public currentTimeEX02()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromSeconds(1), timer); //1초 마다 timer를 호출함
+        }
+        private bool timer()
+        {
+            label1.Text = $"{DateTime.Now}";
+            label2.Text = $"이번 년도는 {DateTime.Now.Year}";
+            label3.Text = $"이번 월은 {DateTime.Now.Month}";
+            label4.Text = $"이번 일은 {DateTime.Now.Day}";
+            label5.Text = $"시간은? {DateTime.Now.TimeOfDay}";
+
+            return true;
+        }
+    }
+}
